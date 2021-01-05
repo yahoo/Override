4d2753ad4b5d7547585c6f523e7f632332dc4416
Eli Liebman <eli.liebman@verizonmedia.com>
2021-01-05 12:22:33 -0500

    - Refactors the way dynamic features are added - Guards against conflicts with static and other dynamic features - Updated tests

--- Example-Swift/Tests/DynamicFeatureRegistrySpec.swift
+++ Example-Swift/Tests/DynamicFeatureRegistrySpec.swift
@@ -25,9 +25,18 @@ class DynamicFeatureRegistrySpec: QuickSpec {
                 class TestRegistry: DynamicFeatureRegistry { }
                 let registry = TestRegistry(withFeatureStore: nil)
                 expect(registry.features).to(beEmpty())
-                let _ = registry.add(feature: Feature(key: "TEST_1"))
+                try! registry.add(feature: Feature(key: "TEST_1"))
                 let featureItem = registry.features.first
                 expect(featureItem?.label).to(equal("TEST_1"))
+            }
+
+            it("Retrieves a dynamic feature") {
+                class TestRegistry: DynamicFeatureRegistry { }
+                let registry = TestRegistry(withFeatureStore: nil)
+                expect(registry.features).to(beEmpty())
+                try! registry.add(feature: Feature(key: "TEST_1"))
+                let featureItem = registry.dynamicFeature(with: "TEST_1")
+                expect(featureItem?.key).to(equal("TEST_1"))
             }
 
             it("Sets dynamic feature override state from the store") {
@@ -38,13 +47,17 @@ class DynamicFeatureRegistrySpec: QuickSpec {
                 let registry = TestRegistry(withFeatureStore: store)
                 expect(registry.features).to(beEmpty())
 
-                let feature1 = registry.add(feature: Feature(key: "TEST_1"))
-                let feature2 = registry.add(feature: Feature(key: "TEST_2"))
-                let feature3 = registry.add(feature: Feature(key: "TEST_3"))
+                try! registry.add(feature: Feature(key: "TEST_1"))
+                try! registry.add(feature: Feature(key: "TEST_2"))
+                try! registry.add(feature: Feature(key: "TEST_3"))
 
-                expect(feature1.override).to(equal(OverrideState.enabled))
-                expect(feature2.override).to(equal(OverrideState.disabled))
-                expect(feature3.override).to(equal(OverrideState.featureDefault))
+                let feature1 = registry.dynamicFeature(with: "TEST_1")
+                let feature2 = registry.dynamicFeature(with: "TEST_2")
+                let feature3 = registry.dynamicFeature(with: "TEST_3")
+
+                expect(feature1!.override).to(equal(OverrideState.enabled))
+                expect(feature2!.override).to(equal(OverrideState.disabled))
+                expect(feature3!.override).to(equal(OverrideState.featureDefault))
             }
 
             it("Properly combines static and dynamic feature") {
@@ -56,9 +69,69 @@ class DynamicFeatureRegistrySpec: QuickSpec {
                 let registry = TestRegistry(withFeatureStore: nil)
                 expect(registry.features.count).to(equal(3))
 
-                let _ = registry.add(feature: Feature(key: "TEST_1"))
-                let _ = registry.add(feature: Feature(key: "TEST_2"))
+                try! registry.add(feature: Feature(key: "TEST_1"))
+                try! registry.add(feature: Feature(key: "TEST_2"))
                 expect(registry.features.count).to(equal(5))
+            }
+
+            it("Throws error when attempting to replace a static feature") {
+                class TestRegistry: DynamicFeatureRegistry {
+                    public let feature1 = Feature()
+                }
+
+                let registry = TestRegistry(withFeatureStore: nil)
+                expect(registry.features.count).to(equal(1))
+
+                let duplicateFeature = Feature(key: "feature1")
+
+                do {
+                    /// Tests that the method throws. We can't really rely on only the catch because that would just test
+                    /// "did the correct error throw" and not "did this actaully throw".
+                    expect (try registry.add(feature: duplicateFeature) ).to(throwError(DynamicFeatureRegistryError.staticFeatureAlreadyExists) )
+                    /// Throw again to clear a warning that the `catch` is never triggered (because the above line handles it).
+                    try registry.add(feature: duplicateFeature)
+                } catch {
+                    let error = error as? DynamicFeatureRegistryError
+                    expect(error).to(equal(.staticFeatureAlreadyExists))
+                }
+
+                expect(registry.features.count).to(equal(1))
+            }
+
+            it("Does not replace dynamic feature") {
+                class TestRegistry: DynamicFeatureRegistry { }
+                let registry = TestRegistry(withFeatureStore: nil)
+                expect(registry.features).to(beEmpty())
+                let feature1 = Feature(key: "TEST_1", requiresRestart: false, defaultState: false)
+                let feature2 = Feature(key: "TEST_1", requiresRestart: true, defaultState: true)
+
+                try! registry.add(feature: feature1)
+                expect(registry.features.count).to(equal(1))
+
+                try! registry.add(feature: feature2)
+                expect(registry.features.count).to(equal(1))
+
+                let storedFeature = registry.dynamicFeature(with: "TEST_1")!
+                expect(storedFeature.defaultState).to(equal(feature1.defaultState))
+                expect(storedFeature.requiresRestart).to(equal(feature1.requiresRestart))
+            }
+
+            it("Does replace dynamic feature") {
+                class TestRegistry: DynamicFeatureRegistry { }
+                let registry = TestRegistry(withFeatureStore: nil)
+                expect(registry.features).to(beEmpty())
+                let feature1 = Feature(key: "TEST_1", requiresRestart: false, defaultState: false)
+                let feature2 = Feature(key: "TEST_1", requiresRestart: true, defaultState: true)
+
+                try! registry.add(feature: feature1)
+                expect(registry.features.count).to(equal(1))
+
+                try! registry.add(feature: feature2, forced: true)
+                expect(registry.features.count).to(equal(1))
+
+                let storedFeature = registry.dynamicFeature(with: "TEST_1")!
+                expect(storedFeature.defaultState).to(equal(feature2.defaultState))
+                expect(storedFeature.requiresRestart).to(equal(feature2.requiresRestart))
             }
         }
 
--- Source/DynamicFeatureRegistry.swift
+++ Source/DynamicFeatureRegistry.swift
@@ -3,6 +3,10 @@
 
 import Foundation
 
+public enum DynamicFeatureRegistryError: Error {
+    case staticFeatureAlreadyExists
+}
+
 /// DynamicFeatureRegistry a subclass of FeatureRegistry which provides an interface for using
 /// a collection of dynamic features instead of just pre-defined ones. When a feature is added,
 /// there is an attempt to apply an override state from the underlying data FeatureStore.
@@ -12,35 +16,53 @@ import Foundation
     private var dynamicFeatures: [String : AnyFeature] = [:]
 
     override var features: [LabeledItem] {
-        let dynamicLabeledItems = dynamicFeatures.values.reduce(into: []) { (result, feature) in
-            result.append(LabeledFeatureItem(label: feature.key, feature: feature))
+
+        let dynamicLabeledItems = dynamicFeatures.values.map {
+            LabeledFeatureItem(label: $0.key, feature: $0)
         }
         let staticFeatures = super.features
         /// merge both dynamic and static features
-        let allFeatures = dynamicLabeledItems + staticFeatures
+        let allFeatures = staticFeatures + dynamicLabeledItems
         return allFeatures
     }
 
     /// This method is used to add a dynamic feature to the store. It will attempt to
     /// assign an `override` value from the FeatureStore if it was previously set and then stores it
     /// locally for future use.
-    /// - Parameter feature: A new feature
-    /// - Returns: The configured feature
-    public func add(feature: AnyFeature) -> AnyFeature {
-        // Bootstrap the last saved override value, if any
+    ///
+    /// - Parameter feature: A new feature to add to the store
+    /// - Parameter forced: When `true`, if the provided feature conflicts with an already stored dynamic feature, it will
+    /// replace that feature in the store. If `false`, the new feature is ignored. Default value is `false`.
+    /// - Throws: If the provided feature conflicts with an already defined static feature, an exemption will be thrown and
+    /// it will not be added.
+    public func add(feature: AnyFeature, forced: Bool = false) throws {
+
+        /// We can't add a dynamic feature which conflicts which conflicts with a static one.
+        guard !(super.features.contains(where: { return $0.label == feature.key })) else {
+            throw DynamicFeatureRegistryError.staticFeatureAlreadyExists
+        }
+        /// If the feature is already stored and `forced` is `false`, there is no reason to contine
+        guard dynamicFeatures[feature.key] == nil || forced else { return }
+
+        /// Bootstrap the last saved override value, if any
         feature.override = featureStore[feature.key]
 
-        // Store feature in `dynamicFeatures` if needed.
-        if dynamicFeatures[feature.key] == nil {
-            dynamicFeatures[feature.key] = feature
-        }
+        /// Store feature in `dynamicFeatures`
+        dynamicFeatures[feature.key] = feature
 
         if let baseFeature = feature as? BaseFeature {
             baseFeature.overrideChangeHandler = featureDidChangeHandler
-            baseFeature.overrideChangeHandler = {[unowned self] feature in
+            baseFeature.overrideChangeHandler = {[weak self] feature in
+                guard let self = self else { return }
                 self.featureStore[feature.key] = feature.override
             }
         }
-        return feature
+    }
+
+    /// Provides an interface for retrieving a dynamic feature. If no matching features are found, this returns nil.
+    /// - Parameter key: The key used to store the feature
+    /// - Returns: The dynamic feature which matches the provided key.
+    public func dynamicFeature(with key: String) -> AnyFeature? {
+        return dynamicFeatures[key]
     }
 }
